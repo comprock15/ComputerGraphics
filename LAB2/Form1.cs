e9ed06301f8078611975bce482f9509e02627a32
Luizett <92753486+Luizett@users.noreply.github.com>
2024-09-11 12:17:02 +0300

    Add main form and plain forms for tasks

---
+++ LAB2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2
+{
+    public partial class Form1 : Form
+    {
+        MainForm menu;
+        public Form1(MainForm menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu.Show();
+            Close();
+        }
+    }
+}
---
+++ LAB2/Form3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB2
+{
+    public partial class Form3 : Form
+    {
+        MainForm menu;
+        public Form3(MainForm menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu.Show();
+            Close();
+        }
+    }
+}
