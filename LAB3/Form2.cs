5d451ca2dae976c0d0d56314daaa52039fd56973
Lisa <edombaian@sfedu.ru>
2024-09-18 21:38:54 +0300

    Add main form and plane task forms for lab 3

---
+++ LAB3/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB3
+{
+    public partial class Form2 : Form
+    {
+        MainForm menu;
+        public Form2(MainForm menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            menu.Show();
+        }
+    }
+}
