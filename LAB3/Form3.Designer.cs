c8c77a1c0b0f5bc81351d641adb3ee8ac13b6002
Lisa <edombaian@sfedu.ru>
2024-09-20 14:21:23 +0300

    Fix panel size change

--- LAB3/Form3.Designer.cs
+++ LAB3/Form3.Designer.cs
@@ -140,6 +140,9 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.pictureBox6);
             this.panel1.Controls.Add(this.pictureBox5);
