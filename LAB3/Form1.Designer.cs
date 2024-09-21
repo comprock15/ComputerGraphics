6fa4b89d8a44291f64dce99b57302b1c32fbd73f
comprock15 <khoruzhaia@sfedu.ru>
2024-09-22 00:21:03 +0300

    Add implementation for task 1b

--- LAB3/Form1.Designer.cs
+++ LAB3/Form1.Designer.cs
@@ -28,13 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.colorChooser = new System.Windows.Forms.Label();
             this.clearButton = new System.Windows.Forms.Button();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
@@ -62,7 +66,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(262, 12);
+            this.radioButton3.Location = new System.Drawing.Point(317, 12);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(150, 21);
             this.radioButton3.TabIndex = 3;
@@ -83,7 +87,7 @@
             // 
             // colorChooser
             // 
-            this.colorChooser.BackColor = System.Drawing.Color.SlateBlue;
+            this.colorChooser.BackColor = System.Drawing.Color.Black;
             this.colorChooser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.colorChooser.Cursor = System.Windows.Forms.Cursors.Hand;
             this.colorChooser.Location = new System.Drawing.Point(13, 40);
@@ -103,11 +107,35 @@
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(317, 40);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(150, 25);
+            this.loadImageButton.TabIndex = 8;
+            this.loadImageButton.Text = "Выбрать файл";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.loadImageButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(474, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(77, 53);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.loadImageButton);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.colorChooser);
             this.Controls.Add(this.pictureBox1);
@@ -120,6 +148,7 @@
             this.Text = "Задание 1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,5 +161,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label colorChooser;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- LAB3/Form1.cs
+++ LAB3/Form1.cs
@@ -14,9 +14,9 @@ namespace LAB3
     {
         MainForm menu;
         Graphics graphics;
-        Bitmap bitmap;
+        Bitmap bitmap, imageToFillFrom;
         Pen pen;
-        Point previousdMouseLocation;
+        Point previousMouseLocation;
 
         public Form1(MainForm menu)
         {
@@ -27,6 +27,7 @@ namespace LAB3
             pictureBox1.Image = bitmap;
             graphics = Graphics.FromImage(bitmap);
             pen = new Pen(colorChooser.BackColor, 1);
+            imageToFillFrom = (Bitmap)pictureBox2.Image;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -51,6 +52,24 @@ namespace LAB3
         }
 
         /// <summary>
+        /// Выбрать изображение для заливки
+        /// </summary>
+        private void loadImageButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Title = "Загрузка изображения";
+                dialog.Filter = "Файлы изображений (*.jpg;*.jpeg;*.png;;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imageToFillFrom = new Bitmap(dialog.FileName);
+                    pictureBox2.Image = imageToFillFrom;
+                }
+            }
+        }
+
+        /// <summary>
         /// Рисование за курсором мыши
         /// </summary>
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -60,9 +79,9 @@ namespace LAB3
                 return;
 
             // Рисуем линию от старых координат мыши к новым
-            graphics.DrawLine(pen, previousdMouseLocation, e.Location);
+            graphics.DrawLine(pen, previousMouseLocation, e.Location);
             // Обновляем последние координаты мыши
-            previousdMouseLocation = e.Location;
+            previousMouseLocation = e.Location;
             // Обновляем отображаемое изображение
             pictureBox1.Invalidate();
         }
@@ -70,12 +89,24 @@ namespace LAB3
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
             // Обновляем последние координаты мыши
-            previousdMouseLocation = e.Location;
+            previousMouseLocation = e.Location;
 
-            // Если включен режим заливки - заливаем область
+            // Включен режим заливки цветом
             if (radioButton2.Checked)
             {
                 FillByColor(e.X, e.Y, bitmap.GetPixel(e.X, e.Y));
+            }
+            // Включен режим заливки изображением
+            else if (radioButton3.Checked)
+            {
+                // Если изображение для заливки отсутствует - выходим
+                if (imageToFillFrom == null)
+                {
+                    loadImageButton.Select();
+                    return;
+                }
+                // Заливаем область, считая, что в месте клика будет центр изображения-заливки
+                FillByImage(e.X, e.Y, imageToFillFrom.Width/2, imageToFillFrom.Height/2, bitmap.GetPixel(e.X, e.Y));
             }
         }
 
@@ -92,26 +123,26 @@ namespace LAB3
                 return;
 
             // Ищем левую границу
-            int left_boundary = x - 1;
-            while (left_boundary >= 0 && bitmap.GetPixel(left_boundary, y) == oldColor)
+            int leftBoundary = x - 1;
+            while (leftBoundary >= 0 && bitmap.GetPixel(leftBoundary, y) == oldColor)
             {
-                --left_boundary;
+                --leftBoundary;
             }
 
             // Ищем правую границу
-            int right_boundary = x + 1;
-            while (right_boundary < bitmap.Width && bitmap.GetPixel(right_boundary, y) == oldColor)
+            int rightBoundary = x + 1;
+            while (rightBoundary < bitmap.Width && bitmap.GetPixel(rightBoundary, y) == oldColor)
             {
-                ++right_boundary;
+                ++rightBoundary;
             }
 
             // Рисуем линию от левой границы до правой границы, не включая саму границу
-            graphics.DrawLine(pen, left_boundary + 1, y, right_boundary - 1, y);
+            graphics.DrawLine(pen, leftBoundary + 1, y, rightBoundary - 1, y);
 
             // Вызываем эту же функцию рекурсивно для всех точек, лежащих выше текущей на один пиксель
             if (y + 1 < bitmap.Height)
             {
-                for (int xi = left_boundary + 1; xi < right_boundary; ++xi)
+                for (int xi = leftBoundary + 1; xi < rightBoundary; ++xi)
                 {
                     FillByColor(xi, y + 1, oldColor);
                 }
@@ -120,7 +151,7 @@ namespace LAB3
             // Вызываем эту же функцию рекурсивно для всех точек, лежащих ниже текущей на один пиксель
             if (y - 1 >= 0)
             {
-                for (int xi = left_boundary + 1; xi < right_boundary; ++xi)
+                for (int xi = leftBoundary + 1; xi < rightBoundary; ++xi)
                 {
                     FillByColor(xi, y - 1, oldColor);
                 }
@@ -128,6 +159,112 @@ namespace LAB3
 
             // Обновляем отображаемое изображение
             pictureBox1.Invalidate();
+        }
+
+        private void FillByImage(int x, int y, int imageX, int imageY, Color oldColor)
+        {
+            // Выходим, если пиксель цвета, который не нужно перекрашивать
+            if (bitmap.GetPixel(x, y) != oldColor)
+                return;
+
+            // Ищем левую границу
+            int leftBoundary = x - 1;
+            while (leftBoundary >= 0 && bitmap.GetPixel(leftBoundary, y) == oldColor)
+            {
+                --leftBoundary;
+            }
+
+            // Ищем правую границу
+            int rightBoundary = x + 1;
+            while (rightBoundary < bitmap.Width && bitmap.GetPixel(rightBoundary, y) == oldColor)
+            {
+                ++rightBoundary;
+            }
+
+            // Рисуем линию от левой границы до правой границы, не включая саму границу
+            DrawLineFromImage(imageToFillFrom, bitmap, leftBoundary + 1, rightBoundary - 1, y, imageX - x + leftBoundary + 1, imageY);
+
+            // Вызываем эту же функцию рекурсивно для всех точек, лежащих выше текущей на один пиксель
+            if (y + 1 < bitmap.Height)
+            {
+                for (int xi = leftBoundary + 1; xi < rightBoundary; ++xi)
+                {
+                    FillByImage(xi, y + 1, imageX - x + xi, imageY + 1, oldColor);
+                }
+            }
+
+            // Вызываем эту же функцию рекурсивно для всех точек, лежащих ниже текущей на один пиксель
+            if (y - 1 >= 0)
+            {
+                for (int xi = leftBoundary + 1; xi < rightBoundary; ++xi)
+                {
+                    FillByImage(xi, y - 1, imageX - x + xi, imageY - 1, oldColor);
+                }
+            }
+
+            // Обновляем отображаемое изображение
+            pictureBox1.Invalidate();
+        }
+
+        /// <summary>
+        /// Пересчитывает координату X так, чтобы она была больше нуля и меньше ширины изображения
+        /// </summary>
+        /// <param name="imageX">Координата X для преобразования</param>
+        /// <param name="image">Изображение</param>
+        /// <returns>Координата X в пределах изображения</returns>
+        private int CalculateCyclicX(int imageX, Bitmap image)
+        {
+            if (imageX < 0)
+            {
+                ++imageX;
+                imageX *= -1;
+                imageX = image.Width - imageX % image.Width - 1;
+            }
+            else if (imageX >= image.Width)
+            {
+                imageX = imageX % image.Width;
+            }
+            return imageX;
+        }
+
+        /// <summary>
+        /// Пересчитывает координату Y так, чтобы она была больше нуля и меньше ширины изображения
+        /// </summary>
+        /// <param name="imageY">Координата Y для преобразования</param>
+        /// <param name="image">Изображение</param>
+        /// <returns>Координата Y в пределах изображения</returns>
+        private int CalculateCyclicY(int imageY, Bitmap image)
+        {
+            if (imageY < 0)
+            {
+                ++imageY;
+                imageY *= -1;
+                imageY = image.Height - imageY % image.Height - 1;
+            }
+            else if (imageY >= image.Height)
+            {
+                imageY = imageY % image.Height;
+            }
+            return imageY;
+        }
+
+        /// <summary>
+        /// Рисует линию на основе изображения-заливки
+        /// </summary>
+        /// <param name="imageFrom">Изображение, которым заливаем</param>
+        /// <param name="imageTo">Изображение, которое заливаем</param>
+        /// <param name="x1">X самого левого пикселя, который нужно перекрасить</param>
+        /// <param name="x2">X самого правого пикселя, который нужно перекрасить</param>
+        /// <param name="y">Y пикселя, который нужно перекрасить</param>
+        /// <param name="ix">X пикселя на изображении-заливке, соответствующий x1</param>
+        /// <param name="iy">Y пикселя на изображении-заливке, соответствующий y</param>
+        private void DrawLineFromImage(Bitmap imageFrom, Bitmap imageTo, int x1, int x2, int y, int ix, int iy)
+        {
+            iy = CalculateCyclicY(iy, imageFrom);
+            for (int x = x1; x <= x2; ++x)
+            {
+                imageTo.SetPixel(x, y, imageFrom.GetPixel(CalculateCyclicX(ix + x - x1, imageFrom), iy));
+            }
         }
 
         /// <summary>
