7a4226f82e81c139085f59d94d93a7d5f3ec01c5
comprock15 <khoruzhaia@sfedu.ru>
2024-09-21 20:57:19 +0300

    Add implementation for task 1a

--- LAB3/Form1.Designer.cs
+++ LAB3/Form1.Designer.cs
@@ -28,21 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorChooser = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(100, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Рисование";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(120, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(135, 21);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Заливка цветом";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(262, 12);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(150, 21);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Заливка рисунком";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1042, 471);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // colorChooser
+            // 
+            this.colorChooser.BackColor = System.Drawing.Color.SlateBlue;
+            this.colorChooser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorChooser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.colorChooser.Location = new System.Drawing.Point(13, 40);
+            this.colorChooser.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.colorChooser.Name = "colorChooser";
+            this.colorChooser.Size = new System.Drawing.Size(242, 25);
+            this.colorChooser.TabIndex = 6;
+            this.colorChooser.Click += new System.EventHandler(this.colorChooser_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(955, 40);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 25);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.colorChooser);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form2";
+            this.Text = "Задание 1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label colorChooser;
+        private System.Windows.Forms.Button clearButton;
     }
 }
--- LAB3/Form1.cs
+++ LAB3/Form1.cs
@@ -13,15 +13,130 @@ namespace LAB3
     public partial class Form1 : Form
     {
         MainForm menu;
+        Graphics graphics;
+        Bitmap bitmap;
+        Pen pen;
+        Point previousdMouseLocation;
+
         public Form1(MainForm menu)
         {
             InitializeComponent();
             this.menu = menu;
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+            graphics = Graphics.FromImage(bitmap);
+            pen = new Pen(colorChooser.BackColor, 1);
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             menu.Show();
+        }
+
+        /// <summary>
+        /// Выбрать цвет для рисования
+        /// </summary>
+        private void colorChooser_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Обновляем цвет
+                    (sender as Label).BackColor = colorDialog.Color;
+                    pen.Color = colorDialog.Color;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Рисование за курсором мыши
+        /// </summary>
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Если не нажата ЛКМ или не режим рисования - выходим
+            if (e.Button != MouseButtons.Left || !radioButton1.Checked)
+                return;
+
+            // Рисуем линию от старых координат мыши к новым
+            graphics.DrawLine(pen, previousdMouseLocation, e.Location);
+            // Обновляем последние координаты мыши
+            previousdMouseLocation = e.Location;
+            // Обновляем отображаемое изображение
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Обновляем последние координаты мыши
+            previousdMouseLocation = e.Location;
+
+            // Если включен режим заливки - заливаем область
+            if (radioButton2.Checked)
+            {
+                FillByColor(e.X, e.Y, bitmap.GetPixel(e.X, e.Y));
+            }
+        }
+
+        /// <summary>
+        /// Заливка линиями заданным цветом
+        /// </summary>
+        /// <param name="x">Координата X перекрашиваемого пикселя</param>
+        /// <param name="y">Координата Y перекрашиваемого пикселя</param>
+        /// <param name="oldColor">Перекрашиваемый цвет</param>
+        private void FillByColor(int x, int y, Color oldColor)
+        {
+            // Выходим, если пиксель уже перекрашен или цвета, который не нужно перекрашивать
+            if (bitmap.GetPixel(x, y) == pen.Color || bitmap.GetPixel(x, y) != oldColor)
+                return;
+
+            // Ищем левую границу
+            int left_boundary = x - 1;
+            while (left_boundary >= 0 && bitmap.GetPixel(left_boundary, y) == oldColor)
+            {
+                --left_boundary;
+            }
+
+            // Ищем правую границу
+            int right_boundary = x + 1;
+            while (right_boundary < bitmap.Width && bitmap.GetPixel(right_boundary, y) == oldColor)
+            {
+                ++right_boundary;
+            }
+
+            // Рисуем линию от левой границы до правой границы, не включая саму границу
+            graphics.DrawLine(pen, left_boundary + 1, y, right_boundary - 1, y);
+
+            // Вызываем эту же функцию рекурсивно для всех точек, лежащих выше текущей на один пиксель
+            if (y + 1 < bitmap.Height)
+            {
+                for (int xi = left_boundary + 1; xi < right_boundary; ++xi)
+                {
+                    FillByColor(xi, y + 1, oldColor);
+                }
+            }
+
+            // Вызываем эту же функцию рекурсивно для всех точек, лежащих ниже текущей на один пиксель
+            if (y - 1 >= 0)
+            {
+                for (int xi = left_boundary + 1; xi < right_boundary; ++xi)
+                {
+                    FillByColor(xi, y - 1, oldColor);
+                }
+            }
+
+            // Обновляем отображаемое изображение
+            pictureBox1.Invalidate();
+        }
+
+        /// <summary>
+        /// Очистить рисунок
+        /// </summary>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            pictureBox1.Invalidate();
         }
     }
 }
