5e10762f8bf84ed251b133d38e019995dc41c4f8
Lisa <edombaian@sfedu.ru>
2024-10-09 15:24:47 +0300

    Add template form for labs

---
+++ PlainFrom/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlainFrom
+{
+    public partial class Form1 : Form
+    {
+        MainForm menu;
+        public Form1(MainForm menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            menu.Show();
+        }
+    }
+}
---
+++ PlainFrom/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlainFrom
+{
+    public partial class Form2 : Form
+    {
+        MainForm menu;
+        public Form2(MainForm menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            menu.Show();
+        }
+    }
+}
