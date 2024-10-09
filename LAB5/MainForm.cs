2e684ccef3d27c09f9ac16f3ce3efe5b285267db
Lisa <edombaian@sfedu.ru>
2024-10-09 15:36:08 +0300

    Add project for lab5

---
+++ LAB5/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form1 f = new Form1(this);
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form2 f = new Form2(this);
+            f.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form3 f = new Form3(this);
+            f.Show();
+        }
+    }
+}
