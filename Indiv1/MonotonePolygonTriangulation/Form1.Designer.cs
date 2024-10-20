6c24b01ad605237cce7e9f348bd0dd5f464069a3
comprock15 <khoruzhaia@sfedu.ru>
2024-10-20 19:24:20 +0300

    Add polygon drawing

---
+++ Indiv1/MonotonePolygonTriangulation/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace MonotonePolygonTriangulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.triangulateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 395);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // triangulateButton
+            // 
+            this.triangulateButton.Location = new System.Drawing.Point(12, 12);
+            this.triangulateButton.Name = "triangulateButton";
+            this.triangulateButton.Size = new System.Drawing.Size(125, 25);
+            this.triangulateButton.TabIndex = 1;
+            this.triangulateButton.Text = "Триангуляция";
+            this.triangulateButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(663, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(125, 25);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Очистка";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.triangulateButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button triangulateButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Indiv1/MonotonePolygonTriangulation/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonotonePolygonTriangulation
+{
+    public partial class Form1 : Form
+    {
+        Polygon polygon = new Polygon();
+        Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                polygon.vertices.Add(e.Location);
+                graphics.Clear(Color.White);
+
+                DrawPolygon(polygon);
+            }
+        }
+
+        private void DrawPolygon(Polygon polygon)
+        {
+            if (polygon.vertices.Count > 1)
+            {
+                graphics.DrawLines(new Pen(Color.Black), polygon.vertices.ToArray());
+                graphics.DrawLine(new Pen(Color.Black), polygon.vertices.First(), polygon.vertices.Last());
+            }
+
+            foreach (PointF p in polygon.vertices)
+                graphics.FillEllipse(new SolidBrush(Color.Black), p.X - 2, p.Y - 2, 4, 4);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            polygon.vertices.Clear();
+            graphics.Clear(Color.White);
+            pictureBox1.Invalidate();
+        }
+    }
+}
