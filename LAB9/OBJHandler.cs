50a5f946a4f84b602c38a16ec7e4fb67dfc6de84
Karbin_Drem <92390626+mpustovoi@users.noreply.github.com>
2024-12-03 00:08:26 +0300

    Texture coord & normal indices for faces
    Added a solution for storing faces with texture coordinate indices and normal indices for each face.
    Several auxiliary methods have been added. (Not used yet)

--- LAB9/OBJHandler.cs
+++ LAB9/OBJHandler.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -23,32 +20,40 @@ namespace LAB9 {
             var normals = new List<Vertex>();
             var textureCoordinates = new List<Vertex>();
 
+            var faceTextureIndices = new List<List<int>>();
+            var faceNormalIndices = new List<List<int>>();
+
             foreach (var line in File.ReadLines(filePath)) {
                 string[] parts = line.Split(' ');
 
-                if (parts[0] == "v") { // вершина
-                    double x = double.Parse(parts[1].Replace('.', ','));
-                    double y = double.Parse(parts[2].Replace('.', ','));
-                    double z = double.Parse(parts[3].Replace('.', ','));
-                    vertices.Add(new Vertex(x, y, z));
-                    edges.Add(new List<int>());
-                }
-                else if (parts[0] == "vn") { // нормаль
-                    double nx = double.Parse(parts[1].Replace('.', ','));
-                    double ny = double.Parse(parts[2].Replace('.', ','));
-                    double nz = double.Parse(parts[3].Replace('.', ','));
-                    normals.Add(new Vertex(nx, ny, nz));
-                }
-                else if (parts[0] == "vt") { // текстурные координаты
-                    var us = parts[1].Replace('.', ',');
-                    var uv = parts[2].Replace('.', ',');
-                    double u = double.Parse(parts[1].Replace('.', ','));
-                    double v = double.Parse(parts[2].Replace('.', ','));
-                    textureCoordinates.Add(new Vertex(0, 0, 0, u, v));
-                }
-                else if (parts[0] == "f") { // Грань
-                    var faceVertices = parts.Skip(1)
-                        .Select(p => {
+                switch (parts[0]) {
+                    case "v": // Вершина
+                        double x = double.Parse(parts[1].Replace('.', ','));
+                        double y = double.Parse(parts[2].Replace('.', ','));
+                        double z = double.Parse(parts[3].Replace('.', ','));
+                        vertices.Add(new Vertex(x, y, z));
+                        edges.Add(new List<int>());
+                        break;
+
+                    case "vn": // Нормаль
+                        double nx = double.Parse(parts[1].Replace('.', ','));
+                        double ny = double.Parse(parts[2].Replace('.', ','));
+                        double nz = double.Parse(parts[3].Replace('.', ','));
+                        normals.Add(new Vertex(nx, ny, nz));
+                        break;
+
+                    case "vt": // Текстурные координаты
+                        double u = double.Parse(parts[1].Replace('.', ','));
+                        double v = double.Parse(parts[2].Replace('.', ','));
+                        textureCoordinates.Add(new Vertex(0, 0, 0, u, v));
+                        break;
+
+                    case "f": // Грань
+                        var faceVertices = new List<int>();
+                        var faceTexIndices = new List<int>();
+                        var faceNormIndices = new List<int>();
+
+                        foreach (var p in parts.Skip(1)) {
                             var vertexIndices = p.Split('/');
                             int vertexIndex = int.Parse(vertexIndices[0]) - 1;
                             int textureIndex = vertexIndices.Length > 1 && !string.IsNullOrEmpty(vertexIndices[1])
@@ -56,28 +61,25 @@ namespace LAB9 {
                             int normalIndex = vertexIndices.Length > 2 && !string.IsNullOrEmpty(vertexIndices[2])
                                 ? int.Parse(vertexIndices[2]) - 1 : -1;
 
-                            return (vertexIndex, textureIndex, normalIndex);
-                        })
-                        .Where(t => t.vertexIndex >= 0 && t.vertexIndex < vertices.Count)
-                        .ToList();
+                            faceVertices.Add(vertexIndex);
+                            faceTexIndices.Add(textureIndex);
+                            faceNormIndices.Add(normalIndex);
+                        }
 
-                    for (int i = 0; i < faceVertices.Count; ++i) {
-                        int start = faceVertices[i].vertexIndex;
-                        int end = faceVertices[(i + 1) % faceVertices.Count].vertexIndex;
-                        if (!edges[start].Contains(end)) edges[start].Add(end);
-                        if (!edges[end].Contains(start)) edges[end].Add(start);
-                    }
+                        faces.Add(faceVertices);
+                        faceTextureIndices.Add(faceTexIndices);
+                        faceNormalIndices.Add(faceNormIndices);
 
-                    faces.Add(faceVertices.Select(fv => fv.vertexIndex).ToList());
+                        for (int i = 0; i < faceVertices.Count; ++i) {
+                            int start = faceVertices[i];
+                            int end = faceVertices[(i + 1) % faceVertices.Count];
+                            if (!edges[start].Contains(end)) edges[start].Add(end);
+                            if (!edges[end].Contains(start)) edges[end].Add(start);
+                        }
+                        break;
                 }
             }
-            Polyhedron polyhedron = normals.Count > 0
-                ? new Polyhedron(vertices, edges, faces, normals, textureCoordinates)
-                : new Polyhedron(vertices, edges, faces);
-
-            foreach (var face in faces) {
-                Console.WriteLine(string.Join(", ", face));
-            }
+            var polyhedron = new Polyhedron(vertices, edges, faces, normals, textureCoordinates, faceTextureIndices, faceNormalIndices);
 
             polyhedron.SetName(Path.GetFileNameWithoutExtension(filePath));
             return polyhedron;
@@ -104,21 +106,26 @@ namespace LAB9 {
                     foreach (var texCoord in polyhedron.textureCoordinates)
                         writer.WriteLine(texCoord.ToOBJTextureString());
 
-                // Сохраняем грани (пока неверно, заглушка)
-                foreach (var face in polyhedron.faces) {
+                // Сохраняем грани
+                for (int i = 0; i < polyhedron.faces.Count; ++i) {
+                    var face = polyhedron.faces[i];
+                    var textureIndices = polyhedron.GetTextureIndices(i);
+                    var normalIndices = polyhedron.GetNormalIndices(i);
+
                     var faceLine = "f";
-                    foreach (var vertexIndex in face) {
-                        int textureIndex = (polyhedron.textureCoordinates?.Count > vertexIndex) ? vertexIndex + 1 : 0;
-                        int normalIndex = (polyhedron.normals?.Count > vertexIndex) ? vertexIndex + 1 : 0;
+                    for (int j = 0; j < face.Count; ++j) {
+                        int vertexIndex = face[j] + 1;
+                        int textureIndex = (textureIndices.Count > j && textureIndices[j] >= 0) ? textureIndices[j] + 1 : 0;
+                        int normalIndex = (normalIndices.Count > j && normalIndices[j] >= 0) ? normalIndices[j] + 1 : 0;
 
                         if (textureIndex > 0 && normalIndex > 0)
-                            faceLine += $" {vertexIndex + 1}/{textureIndex}/{normalIndex}";
+                            faceLine += $" {vertexIndex}/{textureIndex}/{normalIndex}";
                         else if (textureIndex > 0)
-                            faceLine += $" {vertexIndex + 1}/{textureIndex}";
+                            faceLine += $" {vertexIndex}/{textureIndex}";
                         else if (normalIndex > 0)
-                            faceLine += $" {vertexIndex + 1}//{normalIndex}";
+                            faceLine += $" {vertexIndex}//{normalIndex}";
                         else
-                            faceLine += $" {vertexIndex + 1}";
+                            faceLine += $" {vertexIndex}";
                     }
                     writer.WriteLine(faceLine);
                 }
